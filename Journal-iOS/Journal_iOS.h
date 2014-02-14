7b22b87d46df67c868853b738f49d160f2d36eb9
Victor Shamanov <wiruzx@gmail.com>
2014-02-14 18:49:21 +0400

    Static library generation

---
+++ Journal-iOS/Journal_iOS.h
@@ -0,0 +1,13 @@
+//
+//  Journal_iOS.h
+//  Journal-iOS
+//
+//  Created by Виктор Шаманов on 2/14/14.
+//  Copyright (c) 2014 Applifto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Journal_iOS : NSObject
+
+@end
---
+++ Journal-iOS/Journal_iOS.m
@@ -0,0 +1,13 @@
+//
+//  Journal_iOS.m
+//  Journal-iOS
+//
+//  Created by Виктор Шаманов on 2/14/14.
+//  Copyright (c) 2014 Applifto. All rights reserved.
+//
+
+#import "Journal_iOS.h"
+
+@implementation Journal_iOS
+
+@end
